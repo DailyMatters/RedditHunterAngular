79b1f6cb325e37be05815369b292ff94e1131f88
Cláudio Ribeiro <claudio.ribeiro@gatewit.com>
2015-10-28 14:32:57 +0000

    Devs

--- AngularHunter/searchController.js
+++ AngularHunter/searchController.js
@@ -1,8 +1,11 @@
 'use strict';
 
+//http://spin.atomicobject.com/2013/11/22/pass-rails-data-angularjs/
+
 angular.module('searchApp', [])
   .controller('searchController', function($scope, $http){
     var pendingTask;
+	 $scope.items = [25, 50, 75, 100];
 
     if($scope.search === undefined){
       $scope.search = "Entrepreneur";
@@ -16,8 +19,7 @@ angular.module('searchApp', [])
       pendingTask = setTimeout(fetch, 800);
     };
 
-    function fetch(){
-	   
+    function fetch(){	   
 	   $http.get("http://www.reddit.com/r/entrepreneur/search.json?q=" + $scope.search + "&limit=2")
        .success(function(response){ $scope.results = response; });
     }
@@ -31,6 +33,6 @@ angular.module('searchApp', [])
 	//Change or delete this
     $scope.select = function(){
       this.setSelectionRange(0, this.value.length);
-    }	
+    }
 	
   });
